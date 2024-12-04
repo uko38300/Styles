f5e6b4a5cc80eabd0d0ffacee52be1a860ef3ad5
uko38300 <UKO38300@educastur.es>
2024-12-04 09:11:02 +0100

    Proyecto terminado

---
+++ StylesDemo/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace StylesDemo;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		Application
+			.Current!
+			.Resources
+			.TryGetValue("specialButton", out var specialStyle);
+
+		Resources["dynamicResource"] = specialStyle;
+
+    }
+
+    
+}
+
+
+
